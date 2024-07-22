cb3bd1b8272881470aaeda2c1d30c4f87d958af7
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2024-07-22 21:15:49 +0530

    change: colorscheme-kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -88,25 +88,25 @@
 --}
 --
 -- Kanagawa Theme (Original)
--- return {
---   -- https://github.com/rebelot/kanagawa.nvim
---   'rebelot/kanagawa.nvim', -- You can replace this with your favorite colorscheme
---   lazy = false, -- We want the colorscheme to load immediately when starting Neovim
---   priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
---   opts = {
---     -- Replace this with your scheme-specific settings or remove to use the defaults
---     -- transparent = true,
---     background = {
---       -- light = "lotus",
---       dark = "wave", -- "wave, dragon"
---     },
---   },
---   config = function(_, opts)
---     require('kanagawa').setup(opts) -- Replace this with your favorite colorscheme
---     vim.cmd("colorscheme kanagawa") -- Replace this with your favorite colorscheme
---   end
--- }
---
+return {
+  -- https://github.com/rebelot/kanagawa.nvim
+  'rebelot/kanagawa.nvim', -- You can replace this with your favorite colorscheme
+  lazy = false,            -- We want the colorscheme to load immediately when starting Neovim
+  priority = 1000,         -- Load the colorscheme before other non-lazy-loaded plugins
+  opts = {
+    -- Replace this with your scheme-specific settings or remove to use the defaults
+    transparent = true,
+    background = {
+      -- light = "lotus",
+      dark = "wave", -- "wave, dragon"
+    },
+  },
+  config = function(_, opts)
+    require('kanagawa').setup(opts) -- Replace this with your favorite colorscheme
+    vim.cmd("colorscheme kanagawa") -- Replace this with your favorite colorscheme
+  end
+}
+
 -- Catppuccin Theme
 -- return {
 --   -- https://github.com/catppuccin/nvim
@@ -166,14 +166,14 @@
 -- }
 --
 -- solarized Osako
-return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-      require("solarized-osaka")._load("moon")
-    }
-  end,
-}
+-- return {
+--   "craftzdog/solarized-osaka.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent = true,
+--       require("solarized-osaka")._load("moon")
+--     }
+--   end,
+-- }
