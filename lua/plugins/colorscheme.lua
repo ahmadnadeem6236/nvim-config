f0a22990829838b42938e85ba479c82d79610e2a
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2024-07-22 21:07:50 +0530

    change: colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -149,18 +149,31 @@
 -- }
 
 --Tokyo Night Theme
+-- return {
+--   -- https://github.com/folke/tokyonight.nvim
+--   'folke/tokyonight.nvim', -- You can replace this with your favorite colorscheme
+--   lazy = false,            -- We want the colorscheme to load immediately when starting Neovim
+--   priority = 1000,         -- Load the colorscheme before other non-lazy-loaded plugins
+--   opts = {
+--     -- Replace this with your scheme-specific settings or remove to use the defaults
+--     transparent = true,
+--     style = "night", -- other variations "storm, night, moon, day"
+--   },
+--   config = function(_, opts)
+--     require('tokyonight').setup(opts) -- Replace this with your favorite colorscheme
+--     vim.cmd("colorscheme tokyonight") -- Replace this with your favorite colorscheme
+--   end
+-- }
+--
+-- solarized Osako
 return {
-  -- https://github.com/folke/tokyonight.nvim
-  'folke/tokyonight.nvim', -- You can replace this with your favorite colorscheme
-  lazy = false,            -- We want the colorscheme to load immediately when starting Neovim
-  priority = 1000,         -- Load the colorscheme before other non-lazy-loaded plugins
-  opts = {
-    -- Replace this with your scheme-specific settings or remove to use the defaults
-    transparent = true,
-    style = "night", -- other variations "storm, night, moon, day"
-  },
-  config = function(_, opts)
-    require('tokyonight').setup(opts) -- Replace this with your favorite colorscheme
-    vim.cmd("colorscheme tokyonight") -- Replace this with your favorite colorscheme
-  end
+  "craftzdog/solarized-osaka.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+      require("solarized-osaka")._load("moon")
+    }
+  end,
 }
